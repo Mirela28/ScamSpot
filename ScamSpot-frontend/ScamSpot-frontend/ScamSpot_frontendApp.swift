5c73c3246c45983a2e59b0349bfd470295c18bf9
Mirela28 <mirelagirleanu@gmail.com>
2026-03-03 21:56:23 +0100

    Track frontend as normal folder (remove submodule)

---
+++ ScamSpot-frontend/ScamSpot-frontend/ScamSpot_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScamSpot_frontendApp.swift
+//  ScamSpot-frontend
+//
+//  Created by Mirela Girleanu on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScamSpot_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
